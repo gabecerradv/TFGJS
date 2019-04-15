1cc1aecc86e984d915d29dd2ef1a932dc716f3a4
Gabriel Becerra <gabriel.becerra@ezzing.com>
2019-04-15 21:49:41 +0200

    poner nombre

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -7,12 +7,21 @@ import { AuthService} from '../../core/services/auth.service';
   styleUrls: ['../../../assets/css/bootstrap.min.css','../../../assets/css/uncover.css','../../../assets/css/style.css','../../../assets/css/fontawesome-all.min.css','../../../assets/css/set1.css','../../../assets/css/lightbox.css']
 })
 export class NavbarComponent implements OnInit {
-
+  profile: any;
+  id: any;
+  valido: boolean
   constructor(public auth: AuthService) {
     auth.handleAuthentication();
   }
 
   ngOnInit() {
+    this.comprobarUser();
+    this.id = setInterval(() => {
+      this.comprobarUser();
+    }, 500);
+    if (this.valido) {
+      clearInterval(this.id);
+    }
   }
 
   login() {
@@ -22,5 +31,16 @@ export class NavbarComponent implements OnInit {
   logout() {
     this.auth.logout();
   }
-
+  comprobarUser() {
+    if (this.auth.userProfile) {
+      this.profile = this.auth.userProfile;
+    } else {
+      this.auth.getProfile((err, profile) => {
+        this.profile = profile;
+      });
+    }
+    if (this.profile != undefined) {
+      this.valido = true;
+    }
+  }
 }

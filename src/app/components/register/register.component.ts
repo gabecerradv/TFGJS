f25e73ab6772127b6605f26024b38fd3b1a5270d
Gabriel Becerra <gabriel.becerra@ezzing.com>
2019-05-19 19:04:36 +0200

    chore: minor fixes in forms

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {NgForm} from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
     selector: 'app-register',
@@ -17,33 +17,25 @@ export class RegisterComponent implements OnInit {
     ngOnInit() {
     }
 
-    guardar(forma: NgForm) {
+    guardar(forma: NgForm): void {
         if (!this.alertDni && this.contIguales) {
             console.log(forma);
             console.log(forma.value);
         }
     }
 
-    validatePaswords(forma: NgForm) {
+    validatePaswords(forma: NgForm): void {
         let password1 = forma.value.Password1;
         let password2 = forma.value.Password2;
-        if (password1 == password2) {
-            this.contIguales = true;
-        } else {
-            this.contIguales = false;
-        }
+        this.contIguales = password1 == password2;
     }
 
-    validateDni(valor) {
+    validateDni(valor): void {
         let validChars = 'TRWAGMYFPDXBNJZSQVHLCKET';
         let str = valor.value.toString().toUpperCase();
         let letter = str.substr(-1);
         let charIndex = parseInt(str.substr(0, 8)) % 23;
-        if (validChars.charAt(charIndex) === letter) {
-            this.alertDni = false;
-        } else {
-            this.alertDni = true;
-        }
+        this.alertDni = validChars.charAt(charIndex) !== letter;
     }
 
 }
